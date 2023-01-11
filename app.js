4ba0edb20f98513f55d9dd796c5e56fe890156eb
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-01-11 11:35:06 -0500

    complete assignment

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ const userRoutes = require("./routes/users");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(userRoutes);
 
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.use("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3200);
